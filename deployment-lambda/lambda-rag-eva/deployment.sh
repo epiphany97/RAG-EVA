6f4549255513a11904a10d5fef118452e0c41e75
newbeginning <73279725+epiphany97@users.noreply.github.com>
2024-04-22 15:50:04 +0800

    Update deployment.sh

--- deployment-lambda/lambda-rag-eva/deployment.sh
+++ deployment-lambda/lambda-rag-eva/deployment.sh
@@ -3,4 +3,4 @@ account_id=$(aws sts get-caller-identity --query Account --output text)
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $account_id.dkr.ecr.us-west-2.amazonaws.com
 docker build --platform linux/amd64 -t model_eva:test .
 docker tag model_eva:test $account_id.dkr.ecr.us-west-2.amazonaws.com/model_eva:latest
-docker push $account_id.dkr.ecr.us-west-2.amazonaws.com/model-eva:latest
+docker push $account_id.dkr.ecr.us-west-2.amazonaws.com/model_eva:latest
